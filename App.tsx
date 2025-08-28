7b5ba0d4681490b50492178bf779c807f58613f7
veereshnandru75-cyber <your-github-email@example.com>
2025-08-28 12:17:27 +0530

    DEVember

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello world</Text>
+      <Text>DEVember</Text>
       <StatusBar style="auto" />
     </View>
   );
